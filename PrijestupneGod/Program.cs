49c9e1ed9e7fac7489bf8df40f49fb29ea482b74
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-12-06 18:17:30 +0100

    Add files via upload

---
+++ PrijestupneGod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrijestupneGod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1900; i <= 2100; i++)
+            {
+                if (i % 400 == 0 || i % 4 == 0 && i % 100 != 0)
+                {
+                    Console.WriteLine("Prijestupna godina : "+Convert.ToString(i)+".");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
